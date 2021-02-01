a45979999c94aa363b4254c269ffa73a2e1b004e
Jostein Martinsen <jostein.martinsen@cygate.se>
2021-02-01 16:15:06 +0100

    it did not listen on a local port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/libreoffice --nologo --norestore --invisible --nolockcheck --nodefault --headless "$@"
+/usr/bin/libreoffice "-accept=socket,host=127.0.0.1,port=8997,tcpNoDelay=1;urp;" --nologo --norestore --invisible --nolockcheck --nodefault --headless "$@"
 if [ -d "/tmp/hsperfdata_root" ]; then
 	rm -rf /tmp/hsperfdata_root
 fi
